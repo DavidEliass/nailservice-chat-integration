7f6c0b704196859ed8d1514ddf76b6e50a7bf940
DavidEliass <jogadorbr602@gmail.com>
2025-09-09 20:21:01 -0300

    Refactor MySQL connection logic to improve environment variable handling

--- app/database/mysql-connect.js
+++ app/database/mysql-connect.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
-
 let database;
 
 export async function getDatabaseConnection() {
@@ -13,10 +11,15 @@ export async function getDatabaseConnection() {
     }
 
     try {
-        // Use a variável de ambiente para a string de conexão
-        const connection = await mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
-        console.log('String de conexão do MySQL:', process.env.MYSQL_PUBLIC_URL);
+        const publicUrl = process.env.MYSQL_PUBLIC_URL;
+        
+        // Verifique se a variável de ambiente foi carregada.
+        if (!publicUrl) {
+            throw new Error('MYSQL_PUBLIC_URL não está configurada nas variáveis de ambiente.');
+        }
 
+        const connection = await mysql.createConnection(publicUrl);
+        
         console.log('Conexão com o banco de dados MySQL estabelecida.');
         database = connection;
         return database;
